9288fe9298444e4f01d63186380e958f7c3e5999
Rafael Ernesto <scbllu.200@gmail.com>
2025-09-22 15:46:56 -0600

    Agregar seeder para super usuario y actualizar DatabaseSeeder

---
+++ database/seeders/SuperUserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Su_ad;
+
+class SuperUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $superUser = [
+            'name' => 'Administrador',
+            'email' => 'admin@sivarsocial.com',
+            'password' => Hash::make('SuperSecure2025!'),
+            'password_verific_modify' => Hash::make('VerifSecure2025!'),
+            'is_admin' => true,
+            'imagen' => 'img/usuario.svg',
+            'profession' => 'Administrador del Sistema',
+            'last_login' => now(),
+        ];
+
+        Su_ad::create($superUser);
+
+        $this->command->info('Super usuario creado exitosamente.');
+    }
+}
